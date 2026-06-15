5726b0203e761596bad045fc6312c760c1a2069c
Gonzalo Alejandro Tintaya Farfan <202300889h@utea.edu.pe>
2026-06-15 11:27:45 -0500

    refactorizacion 03

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,8 +1,8 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
-        console.log({ movieId });
+    function getAllMovies( id: string ) {
+        console.log({ id });
     }
 
     // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
@@ -11,17 +11,25 @@
     }
 
     // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
-        console.log({ ActorId });
+    function getActorBioById( id: string ) {
+        console.log({ id });
     }
-    
+
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    interface Movie {
+        title: string;
+        description: string;  
+        rating: number;
+        cast: string[];      
+    }
+    function createMovie({ title, description, rating, cast }: Movie) {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+    
+
+    function createActor( fullName: string, birthdate: Date ): boolean {
         
         // tarea asincrona para verificar nombre
         // ..
